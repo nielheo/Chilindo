5aba0ea5d19547607ecca6e6c2b1c4b9ceed5459
niel <niel.heo@gmail.com>
2018-01-04 22:20:08 +0700

    React.UI

---
+++ Chillindo.React/ClientApp/components/Common/FormInput.tsx
@@ -0,0 +1,15 @@
+﻿import * as React from 'react'
+
+export default class FormInput extends React.Component<{ label: string, error: string }, any> {
+  public render() {
+    //console.log(this.props.airlines)
+    return <div className={'form-group ' + (this.props.error ? 'has-error' : '')}>
+      <label className="control-label">{this.props.label}</label>
+      {this.props.children}
+      {
+        this.props.error &&
+        <span className='control-label'>{this.props.error}</span>
+      }
+    </div>
+  }
+}
